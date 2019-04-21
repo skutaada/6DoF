80fe0cba7b348f2dab006804c95e32171205a28b
Adam Skuta <adam.skuta@gmail.com>
2019-04-21 17:42:26 +0000

    added encoding and functionality

--- udp_send.py
+++ udp_send.py
@@ -1,12 +1,14 @@
 import socket
 
-UDP_IP = "127.0.0.1"
+UDP_IP = "10.0.1.11" # concrete IP
 UDP_PORT = 5005
 MESSAGE = "Hello, World!"
 
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
 print("message:", MESSAGE)
+
+MESSAGE = MESSAGE.encode(encoding="utf-8")
 
 sock = socket.socket(socket.AF_INET, # Internet
                     socket.SOCK_DGRAM) # UDP
