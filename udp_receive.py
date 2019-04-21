58e9fca9930e7fe35f8bc59a8587b017598a0e9e
Škuta, Adam <skutaada@fel.cvut.cz>
2019-04-21 19:43:23 +0200

    added functionality and decoding

--- udp_receive.py
+++ udp_receive.py
@@ -1,6 +1,6 @@
 import socket
 
-UDP_IP = "127.0.0.1"
+UDP_IP = ''
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, # Internet
@@ -8,5 +8,9 @@ sock = socket.socket(socket.AF_INET, # Internet
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("receive message", data)
+    try:
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        print("receive message", data.decode(encoding="utf-8"))
+    except Exception:
+        print ('\nExit . . .\n')
+        break
